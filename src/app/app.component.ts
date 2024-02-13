6ffddd0d2302d2ff09db452c9e2a514bca6b72c2
JptmJ <joshiptm797@gmail.com>
2024-02-13 14:10:09 +0530

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'highlighter';
+  backColor: string = 'white'
+  fontColor: string = 'black'
+
+  changeBack() {
+    this.backColor = this.backColor === 'black' ? 'white' : 'black';
+    this.fontColor = this.fontColor === 'white' ? 'black' : 'white';
+  }
 }
